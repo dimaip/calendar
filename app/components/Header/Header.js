9673826f1dab89301a8c2ee89058b259108906db
Dmitri Pisarev <dimaip@gmail.com>
2020-01-29 17:47:31 +0300

    Move out Svgs

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { css } from 'emotion';
 import { Link } from 'react-router-dom';
 
-import CalendarIcon from 'components/CalendarIcon/CalendarIcon.js';
+import CalendarIcon from 'components/Svgs/CalendarIcon';
 import Button from 'components/Button/Button';
+import Cross from 'components/Svgs/Cross';
 
 const Header = ({ handleToggleClick, calendarShown }) => (
     <header
@@ -36,42 +37,7 @@ const Header = ({ handleToggleClick, calendarShown }) => (
                             margin-right: -10px;
                         `}
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="45.828"
-                            height="45.828"
-                            viewBox="0 0 45.828 45.828"
-                        >
-                            <g transform="translate(-322.586 -7.586)">
-                                <g transform="translate(324 9)">
-                                    <path
-                                        d="M15.2,0A15.2,15.2,0,1,1,0,15.2,15.2,15.2,0,0,1,15.2,0Z"
-                                        transform="translate(0 21.5) rotate(-45)"
-                                        fill="none"
-                                        stroke="#201f24"
-                                        strokeWidth="2"
-                                    />
-                                </g>
-                                <g transform="translate(339.732 24.732)">
-                                    <line
-                                        x1="12.585"
-                                        y2="12.585"
-                                        transform="translate(0)"
-                                        fill="none"
-                                        stroke="#201f24"
-                                        strokeWidth="2"
-                                    />
-                                    <line
-                                        x2="12.585"
-                                        y2="12.585"
-                                        transform="translate(0)"
-                                        fill="none"
-                                        stroke="#201f24"
-                                        strokeWidth="2"
-                                    />
-                                </g>
-                            </g>
-                        </svg>
+                        <Cross />
                     </div>
                 ) : (
                     <CalendarIcon />
