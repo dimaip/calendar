1e39913ff719af3abdf1649a38352ec27300e845
Dmitri Pisarev <dimaip@gmail.com>
2020-01-29 17:46:24 +0300

    BottomNav

---
+++ app/components/BottomNav/BottomNav.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { css } from 'emotion';
+import { Link, useParams } from 'react-router-dom';
+import { useTheme } from 'emotion-theming';
+
+const BottomNav = ({ active }) => {
+    const { date } = useParams();
+    const theme = useTheme();
+
+    const itemClass = css`
+        font-size: 14px;
+        padding: 6px 12px;
+        border-bottom: 3px solid white;
+        color: ${theme.colours.gray};
+    `;
+    const activeClass = css`
+        border-bottom-color: ${theme.colours.primary};
+        color: ${theme.colours.darkGray};
+    `;
+
+    return (
+        <div
+            className={css`
+                display: flex;
+                position: sticky;
+                bottom: 0;
+                background-color: white;
+                padding: 0 12px;
+                box-shadow: 0px 0px 5px #bbb;
+            `}
+        >
+            <Link className={itemClass + ' ' + (active === 'calendar' ? activeClass : '')} to={`/date/${date}`}>
+                Календарь
+            </Link>
+            <Link className={itemClass + ' ' + (active === 'bible' ? activeClass : '')} to="/">
+                Библия
+            </Link>
+            <Link className={itemClass + ' ' + (active === 'service' ? activeClass : '')} to="/">
+                Богослужение
+            </Link>
+        </div>
+    );
+};
+export default BottomNav;
