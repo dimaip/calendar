040f3cebb6b59c14ff108d41d74f4ce54619585f
Dmitri Pisarev <dimaip@gmail.com>
2020-02-24 09:05:22 +0300

    Fix case

---
+++ app/components/svgs/Cross.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cross = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 22.2" width="24">
+        <path d="M5 3.7l14 14.7m0-14.7L5 18.4" fill="none" stroke="#000" strokeLinecap="round" strokeMiterlimit="10" />
+    </svg>
+);
+
+export default Cross;
