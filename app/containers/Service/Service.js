8eec9adda2d8f4cc83aa875922b233dd7971f731
Dmitri Pisarev <dimaip@gmail.com>
2020-03-02 15:28:09 +0300

    CSJ

--- app/containers/Service/Service.js
+++ app/containers/Service/Service.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { ThemeProvider } from 'emotion-theming';
 import LeftIcon from 'components/svgs/LeftIcon';
@@ -14,6 +14,8 @@ const Service = () => {
     const { serviceId, date } = useParams();
     const { data: day } = useDay(date);
     const theme = getTheme(day?.colour);
+
+    const [lang, setLang] = useState('default');
 
     return (
         <ThemeProvider theme={theme}>
@@ -51,8 +53,31 @@ const Service = () => {
                         className={css`
                             position: absolute;
                             right: 12px;
+                            display: flex;
+                            align-items: center;
                         `}
                     >
+                        <div
+                            onClick={() => setLang(lang === 'ЦСЯ' ? 'default' : 'ЦСЯ')}
+                            role="button"
+                            className={css`
+                                cursor: pointer;
+                                padding: 8px 6px 3px 6px;
+                                margin-right: 6px;
+                                border-radius: 8px;
+                                color: ${lang === 'ЦСЯ' ? 'white' : 'inherit'};
+                                background-color: ${lang === 'ЦСЯ' ? 'gray' : 'white'};
+                            `}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 400 440" width="16">
+                                <path
+                                    d="M187.57.442c-16.544.166-26.08 1.862-34.412 15.099v170.734s.615 19.322-23.513 19.322H18.043S.546 207.411.546 223.098v130.32s-4.83 12.064 45.84 12.064c50.667 0 53.095-10.849 53.095-10.849v-109.23s-.15-13.832 23.962-13.832c32.134 0 29.715 9.628 29.715 12.644v182.798s-1.016 12.565 46.842 12.565 46.842-12.565 46.842-12.565V244.215c0-3.016-2.42-12.644 29.715-12.644 24.111 0 23.988 13.832 23.988 13.832v109.23s2.402 10.85 53.07 10.85 45.839-12.064 45.839-12.064v-130.32c0-15.688-17.497-17.502-17.497-17.502H270.382c-24.128 0-23.54-19.322-23.54-19.322V15.54C236.65-.651 224.64.428 200.212.44h-.423c-4.46-.002-8.524-.037-12.219 0z"
+                                    fillRule="evenodd"
+                                    fill={lang === 'ЦСЯ' ? 'white' : 'black'}
+                                    strokeWidth=".845"
+                                />
+                            </svg>
+                        </div>
                         <ZoomControlToggle />
                     </div>
                 </div>
@@ -63,7 +88,7 @@ const Service = () => {
                             margin-right: 12px;
                         `}
                     >
-                        <Suspense fallback={Loader}>{serviceId === 'zlatoust' && <Zlatoust />}</Suspense>
+                        <Suspense fallback={Loader}>{serviceId === 'zlatoust' && <Zlatoust lang={lang} />}</Suspense>
                     </div>
                 </Zoom>
             </div>
