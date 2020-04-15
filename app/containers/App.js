3be54e7efcac4b699b12692ca6cce2ef2e1fffc1
Dmitri Pisarev <dimaip@gmail.com>
2020-04-15 14:27:05 +0300

    Add loader screen

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import Routes from '../Routes';
@@ -9,6 +9,14 @@ import { ReactQueryConfigProvider } from 'react-query';
 const queryConfig = { refetchAllOnWindowFocus: false };
 
 export default ({ store }) => {
+    useEffect(() => {
+        const loader = document.getElementById('loader');
+        const reactRoot = document.getElementById('react-root');
+        if (loader && reactRoot) {
+            loader.style.display = 'none';
+            reactRoot.style.display = 'block';
+        }
+    }, []);
     return (
         <ReactQueryConfigProvider config={queryConfig}>
             <Provider store={store}>
