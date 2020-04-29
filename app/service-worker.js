e0b554691e5b92824f2e85b9f5e47e8342893ea8
Dmitri Pisarev <dimaip@gmail.com>
2020-04-29 09:55:40 +0300

    Temp disable service worker

--- app/service-worker.js
+++ app/service-worker.js
@@ -14,15 +14,15 @@ googleAnalytics.initialize();
 
 console.log('Precaching routes:', self.__WB_MANIFEST);
 // Pre-cache main JS assets
-precacheAndRoute(self.__WB_MANIFEST);
+// precacheAndRoute(self.__WB_MANIFEST);
 
 const apiHostRegexp = process.env.API_HOST;
 const publicUrlRegexp = process.env.PUBLIC_URL;
 
 // Cache (pre-caching happens in precache.worker.js) api and static requests
-registerRoute(new RegExp(`^${apiHostRegexp}/clear-cache`), new NetworkOnly());
-registerRoute(new RegExp('/built/version'), new NetworkOnly());
-registerRoute(new RegExp(`^${apiHostRegexp}`), new StaleWhileRevalidate());
-registerRoute(new RegExp('^https://psmb.ru'), new StaleWhileRevalidate());
-registerRoute(new RegExp('/assets/'), new CacheFirst());
-registerRoute(new RegExp(`^${publicUrlRegexp}/[^?].*`), new StaleWhileRevalidate());
+// registerRoute(new RegExp(`^${apiHostRegexp}/clear-cache`), new NetworkOnly());
+// registerRoute(new RegExp('/built/version'), new NetworkOnly());
+// registerRoute(new RegExp(`^${apiHostRegexp}`), new StaleWhileRevalidate());
+// registerRoute(new RegExp('^https://psmb.ru'), new StaleWhileRevalidate());
+// registerRoute(new RegExp('/assets/'), new CacheFirst());
+// registerRoute(new RegExp(`^${publicUrlRegexp}/[^?].*`), new StaleWhileRevalidate());
