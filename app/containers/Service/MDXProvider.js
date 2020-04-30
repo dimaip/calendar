bf237d164a0a5e5afaffb94519e2c06f36290728
Dmitri Pisarev <dimaip@gmail.com>
2020-04-30 16:23:15 +0300

    Fix MDXProvider performance

--- app/containers/Service/MDXProvider.js
+++ app/containers/Service/MDXProvider.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { MDXProvider as OriginalMDXProvider } from '@mdx-js/react';
 
-const MDXProvider = ({ children }) => (
-    <OriginalMDXProvider
-        components={{
-            h1: ({ children }) => <h1 className="_-ОСНОВНОЙ_Имя-Службы">{children}</h1>,
-            h2: ({ children }) => <h2 className="_-ОСНОВНОЙ_Имя-РаздСл">{children}</h2>,
-            h3: ({ children }) => <h3 className="_-ОСНОВНОЙ_Имя-части-отст5">{children}</h3>,
-            p: ({ children }) => {
-                return <p className="_-ОСНОВНОЙ_Основной-отст1-5">{children}</p>;
-            },
-            strong: ({ children }) => <span className="_-ВЫДЕЛЕНИЯ_КРАСНЫЙ">{children}</span>,
-        }}
-    >
-        {children}
-    </OriginalMDXProvider>
-);
+const mapping = {
+    h1: ({ children }) => <h1 className="_-ОСНОВНОЙ_Имя-Службы">{children}</h1>,
+    h2: ({ children }) => <h2 className="_-ОСНОВНОЙ_Имя-РаздСл">{children}</h2>,
+    h3: ({ children }) => <h3 className="_-ОСНОВНОЙ_Имя-части-отст5">{children}</h3>,
+    p: ({ children }) => {
+        return <p className="_-ОСНОВНОЙ_Основной-отст1-5">{children}</p>;
+    },
+    strong: ({ children }) => <span className="_-ВЫДЕЛЕНИЯ_КРАСНЫЙ">{children}</span>,
+};
+
+const MDXProvider = ({ children }) => <OriginalMDXProvider components={mapping}>{children}</OriginalMDXProvider>;
 
 export default MDXProvider;
