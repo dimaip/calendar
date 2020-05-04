2fbd0bc915b730fa37a1c39764eb9b5b1597f9a3
Zira <zaveryukhaira@gmail.com>
2020-05-05 00:35:01 +0300

    TASK Priugotovlenie

---
+++ app/containers/Service/Texts/Liturgies/Katekhumen/Priugotovl/Priugotovl.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PriugotovlRu from './Priugotovl.ru.mdx';
+import PriugotovlCsj from './Priugotovl.csj.mdx';
+
+const langMap = {
+    default: PriugotovlRu,
+    ЦСЯ: PriugotovlCsj,
+};
+
+const Priugotovl = ({ lang = 'default', date: dateString }) => {
+    const Component = langMap[lang];
+    return <Component lang={lang} />;
+};
+
+export default Priugotovl;
