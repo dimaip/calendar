c74adbac461671c90a5087ca13b4f9bc39af73bc
Dmitri Pisarev <dimaip@gmail.com>
2020-10-13 09:52:58 +0300

    Sunday Vhodnoi stih

--- app/containers/Service/Texts/Liturgies/Katekhumen/VhodnoiStih/VhodnoiStih.js
+++ app/containers/Service/Texts/Liturgies/Katekhumen/VhodnoiStih/VhodnoiStih.js
@@ -2,11 +2,20 @@ import React from 'react';
 import MdxLoader from 'containers/Service/Texts/MdxLoader';
 import Parts from 'components/Parts/Parts';
 
+const DefaultVhodnoiStih = ({ date }) => {
+    const dateObj = new Date(date);
+    const dayOfWeek = dateObj.getDay();
+    if (dayOfWeek === 0) {
+        return <MdxLoader src={'Liturgies/Katekhumen/VhodnoiStih/Sunday'} />;
+    }
+    return <MdxLoader src={'Liturgies/Katekhumen/VhodnoiStih/Budni'} />;
+};
+
 const VhodnoiStih = ({ date }) => (
     <Parts
         partNames={['liturgy.Входной стих']}
         date={date}
-        fallback={<MdxLoader src="Liturgies/Katekhumen/VhodnoiStih" />}
+        fallback={<DefaultVhodnoiStih date={date} />}
     />
 );
 
