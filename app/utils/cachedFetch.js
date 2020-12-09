26c72be2e8d091a6bc300d7343bf82f6faab7aac
Dmitri Pisarev <dimaip@gmail.com>
2020-12-09 19:58:52 +0300

    Update cache after request

--- app/utils/cachedFetch.js
+++ app/utils/cachedFetch.js
@@ -1,6 +1,12 @@
 import getWrapper from './StorageAPIWrapper';
 const wrapper = getWrapper();
 
+/**
+ * Make a fetch request and cache it to IndexedDB
+ *
+ * @param {string} url
+ * @param {boolean} prefetch just prefetch the data without returning
+ */
 const cachedFetch = async (url, prefetch = false) => {
     return new Promise(function(resolve, reject) {
         wrapper.openStore({ database: 'pb', table: 'requestsCache' }, res => {
@@ -9,8 +15,26 @@ const cachedFetch = async (url, prefetch = false) => {
             } else {
                 wrapper.getItem(url, value => {
                     if (value) {
+                        // Return saved value
                         resolve(value);
+                        // And re-fetch the data in background (if not prefetching)
+                        if (!prefetch) {
+                            fetch(url)
+                                .then(response => {
+                                    if (!response.ok) {
+                                        throw new Error(response.statusText);
+                                    }
+                                    return response.text();
+                                })
+                                .then(text => {
+                                    // Save to cache
+                                    wrapper.setItem(url, text, () => {});
+                                })
+                                // We swallow errors on update requests
+                                .catch(e => console.error(e));
+                        }
                     } else {
+                        // Request not cached, fetch it
                         fetch(url)
                             .then(response => {
                                 if (!response.ok) {
@@ -19,6 +43,7 @@ const cachedFetch = async (url, prefetch = false) => {
                                 return response.text();
                             })
                             .then(text => {
+                                // Save to cache
                                 wrapper.setItem(url, text, () => {});
                                 resolve(text);
                             })
