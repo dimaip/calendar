45a09a235f0ee3af4b9e85bf8b5f20c1c00d453b
Dmitri Pisarev <dimaip@gmail.com>
2020-12-25 14:52:55 +0300

    Update app/containers/App.js

--- app/containers/App.js
+++ app/containers/App.js
@@ -10,7 +10,7 @@ import recoilPersist from 'recoil-persist';
 
 const { RecoilPersist, updateState } = recoilPersist([
     'scriptEditorIsActive',
-    'iosPrompt',
+    'iosPromptDismissed',
     'zoomControlIsShown',
     'zoomState',
     'langState',
