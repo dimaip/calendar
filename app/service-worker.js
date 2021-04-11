05b1aa13d1fc31497575b1ce25de2d36f325216a
Dmitri Pisarev <dimaip@gmail.com>
2021-04-11 10:45:07 +0300

    Adjust SW cache

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,5 +1,5 @@
 import { precacheAndRoute } from 'workbox-precaching';
-import { StaleWhileRevalidate, CacheFirst, NetworkOnly } from 'workbox-strategies';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
 import { registerRoute } from 'workbox-routing';
 import * as googleAnalytics from 'workbox-google-analytics';
 
@@ -18,12 +18,9 @@ console.log('Precaching routes:', self.__WB_MANIFEST);
 // Pre-cache main JS assets
 precacheAndRoute(self.__WB_MANIFEST);
 
-const apiHostRegexp = process.env.API_HOST;
+// const apiHostRegexp = process.env.API_HOST;
 const publicUrlRegexp = process.env.PUBLIC_URL;
 
-// Cache (pre-caching happens in precache.worker.js) api and static requests
-registerRoute(new RegExp(`^${apiHostRegexp}/clear-cache`), new NetworkOnly());
-registerRoute(new RegExp('/built/version.json'), new NetworkOnly());
-// registerRoute(new RegExp('^https://psmb.ru'), new StaleWhileRevalidate());
+// Cache assets and icons
 registerRoute(new RegExp('/assets/'), new CacheFirst());
 registerRoute(new RegExp(`^${publicUrlRegexp}/.*\\.(png|ico|svg)`), new StaleWhileRevalidate());
