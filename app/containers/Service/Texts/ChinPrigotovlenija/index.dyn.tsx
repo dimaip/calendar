1818fad07361a1d6d6b58ab3062c746d31d4bcae
Dmitri Pisarev <dimaip@gmail.com>
2021-06-21 12:25:55 +0300

    ChinPrigotovlenija adjustments

--- app/containers/Service/Texts/ChinPrigotovlenija/index.dyn.tsx
+++ app/containers/Service/Texts/ChinPrigotovlenija/index.dyn.tsx
@@ -1,12 +1,15 @@
 import { makeIsEasterOffsetRange } from 'domain/getDayInfo';
 
 import React from 'react';
+import useDay from 'hooks/useDay';
 
 import ChinPrigotovlenijaMdx from './ChinPrigotovlenija.mdx';
 
 const ChinPrigotovlenija = ({ date }): JSX.Element => {
     const isEasterOffsetRange = makeIsEasterOffsetRange(date);
-    const props = { date, isEasterOffsetRange };
+    const { data: day } = useDay(date);
+    const greatLent = day?.fastName === 'Великий пост';
+    const props = { date, isEasterOffsetRange, greatLent };
     return <ChinPrigotovlenijaMdx {...props} />;
 };
 export default ChinPrigotovlenija;
