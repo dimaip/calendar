f9861a8a7ff3c7b46d2fce16ec26250547923ce0
Dmitri Pisarev <dimaip@gmail.com>
2021-10-21 09:31:26 +0300

    FEATURE: sermons based on verse

--- app/hooks/useExternalDay.ts
+++ app/hooks/useExternalDay.ts
@@ -1,8 +1,20 @@
 import { useQuery } from 'react-query';
 import cachedFetch from 'utils/cachedFetch';
+import useDay from 'hooks/useDay';
 
-const fetchExternalDay = (date) => cachedFetch(`https://psmb.ru/?calendarDate=${date}`);
+const fetchExternalDay = async (date: string, verses?: string[]): Promise<any> =>
+    cachedFetch(`https://psmb.ru/?calendarDate=${date}${verses ? `&verses=${JSON.stringify(verses)}` : ''}`);
 
-const useExternalDay = (date) => useQuery(['ext-day', { date }], () => fetchExternalDay(date), { retry: false });
+const useExternalDay = (date: string): any => {
+    const dayQuery = useDay(date);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const readings = dayQuery.data?.readings?.Литургия;
+
+    return useQuery(['ext-day', { date }], async () => fetchExternalDay(date, readings), {
+        retry: false,
+        enabled: dayQuery.isFetched,
+    });
+};
 
 export default useExternalDay;
