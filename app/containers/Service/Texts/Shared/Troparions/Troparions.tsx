272d628b456b7aef1f0f7658a0beff0e99562826
Dmitri Pisarev <dimaip@gmail.com>
2021-10-26 13:11:50 +0300

    WeekDay troparions

--- app/containers/Service/Texts/Shared/Troparions/Troparions.tsx
+++ app/containers/Service/Texts/Shared/Troparions/Troparions.tsx
@@ -1,6 +1,23 @@
+import { isHymnsVsednev } from 'domain/getDayInfo';
+
 import React from 'react';
 import Parts from 'components/Parts/Parts';
 import MdxLoader from 'containers/Service/Texts/MdxLoader';
+
+export const WeekdayTroparions = ({ day, date, serviceType }): JSX.Element => {
+    if (serviceType !== 'Литургия') {
+        return null;
+    }
+    const dateObj = new Date(date);
+    const dayOfWeek = dateObj.getDay();
+    if (dayOfWeek === 0) {
+        return null;
+    }
+    if (!isHymnsVsednev(dateObj)) {
+        return null;
+    }
+    return <MdxLoader src={`Shared/Troparions/Day${dayOfWeek}`} />;
+};
 
 export const SundayTroparions = ({ day, date }) => {
     const dateObj = new Date(date);
@@ -20,7 +37,12 @@ const Troparions = ({ day, date, serviceType }) => (
         date={date}
         partNames={['shared.Тропари']}
         alwaysShowFallback
-        fallback={<SundayTroparions day={day} date={date} />}
+        fallback={
+            <>
+                <WeekdayTroparions day={day} date={date} serviceType={serviceType} />
+                <SundayTroparions day={day} date={date} />
+            </>
+        }
         serviceType={serviceType}
     />
 );
