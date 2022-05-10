e39710f1f65a99087011618e142778cb817c9bcf
Dmitri Pisarev <dimaip@gmail.com>
2022-05-10 13:01:10 +0300

    Fix TOC formatting

--- app/components/Typography/Typography.tsx
+++ app/components/Typography/Typography.tsx
@@ -2,20 +2,32 @@ import ScriptEditorInput from 'components/ScriptEditor/ScriptEditorInput';
 import { makeId } from 'hooks/useUpdateTOC';
 import React from 'react';
 
-const addToTOC = (title: unknown): void => {
+const addToTOC = (title: unknown): string => {
     if (!window.TOC) {
         window.TOC = {};
     }
-    const domId = String(title).replace(/\[object Object\]/g, '');
+    const domId =
+        typeof title === 'string'
+            ? title
+            : Array.isArray(title)
+            ? title.filter((i) => typeof i === 'string').join(' ')
+            : null;
+    if (!domId) {
+        return '';
+    }
     window.TOC[domId] = domId;
-    return makeId(domId)
+    return makeId(domId);
 };
 
 export const H1 = ({ children }): JSX.Element => <h1 className="H1">{children}</h1>;
 
 export const H2 = ({ children }): JSX.Element => {
     const domId = addToTOC(children);
-    return <h2 className="H2" id={domId}>{children}</h2>;
+    return (
+        <h2 className="H2" id={domId}>
+            {children}
+        </h2>
+    );
 };
 
 export const H3 = ({ children }): JSX.Element => {
