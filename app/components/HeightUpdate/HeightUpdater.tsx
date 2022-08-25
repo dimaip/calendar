3e0d0b99701228a7fcf8d96e266945211f5f919c
Dmitri Pisarev <dimaip@gmail.com>
2022-08-25 16:51:44 +0300

    Fix height updater

--- app/components/HeightUpdate/HeightUpdater.tsx
+++ app/components/HeightUpdate/HeightUpdater.tsx
@@ -4,7 +4,7 @@ import { SwipeableViewsContext } from 'react-swipeable-views';
 export const HeightUpdater = ({ children }: { children: React.ReactNode }): JSX.Element => {
     const context = useContext(SwipeableViewsContext);
     useEffect(() => {
-        context.slideUpdateHeight();
+        context?.slideUpdateHeight?.();
     });
     return <>{children}</>;
 };
