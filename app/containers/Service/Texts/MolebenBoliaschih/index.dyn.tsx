1444b659a616aa3d2170686282822f04252361fc
Dmitri Pisarev <dimaip@gmail.com>
2022-09-27 10:41:55 +0300

    Finalise Moleben

---
+++ app/containers/Service/Texts/MolebenBoliaschih/index.dyn.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import MolebenBoliaschihMdx from './MolebenBoliaschih.mdx';
+
+const MolebenBoliaschih = (): JSX.Element => <MolebenBoliaschihMdx />;
+export default MolebenBoliaschih;
