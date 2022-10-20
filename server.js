93584612a87918aef8a3c2a1655ab570f4981ff6
Dmitri Pisarev <dimaip@gmail.com>
2022-10-20 09:23:53 +0300

    Disable HMR for now

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.NODE_PORT || 3000;
 if (!isProd) {
     const loadDev = async () => {
         const { default: webpackDevMiddleware } = await import('webpack-dev-middleware');
-        const { default: webpackHotMiddleware } = await import('webpack-hot-middleware');
+        // const { default: webpackHotMiddleware } = await import('webpack-hot-middleware');
         const { default: config } = await import('./webpack.dev.js');
         const compiler = webpack(config);
         app.use(
@@ -17,7 +17,7 @@ if (!isProd) {
             })
         );
         // NOTE: Only the client bundle needs to be passed to `webpack-hot-middleware`.
-        app.use(webpackHotMiddleware(compiler));
+        // app.use(webpackHotMiddleware(compiler));
     };
     loadDev();
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
-import webpack from 'webpack';
+// import webpack from 'webpack';
 import { merge } from 'webpack-merge';
 
 import common from './webpack.base.js';
 
 export default merge(common, {
-    entry: ['babel-polyfill', 'webpack-hot-middleware/client', 'react-hot-loader/patch', 'client.tsx'],
+    entry: ['babel-polyfill', 'client.tsx'],
     mode: 'development',
     devtool: 'eval-source-map',
-    plugins: [new webpack.HotModuleReplacementPlugin()],
+    // plugins: [new webpack.HotModuleReplacementPlugin()],
 });
