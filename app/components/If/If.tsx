1cd303d85bd326103a6c802c29506de2e22a62b5
Dmitri Pisarev <dimaip@gmail.com>
2022-11-01 13:54:11 +0300

    Another go at fixing If

--- app/components/If/If.tsx
+++ app/components/If/If.tsx
@@ -5,9 +5,8 @@ export const Else = ({ children }) => <>{children}</>;
 
 const If = ({ condition, children }) => {
     const childrenArray = React.Children.toArray(children)
-    const thenEl = childrenArray.find(el => el?.type?.name === 'Then')
-    const elseEl = childrenArray.find(el => el?.type?.name === 'Else')
-
+    const thenEl = childrenArray.find(el => el?.type === Then)
+    const elseEl = childrenArray.find(el => el?.type === Else)
 
     if (thenEl || elseEl) {
         if (condition) {
