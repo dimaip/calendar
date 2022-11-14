fbb669bb38ce52fc9d281387d1b8d7ea387e4105
Dmitri Pisarev <dimaip@gmail.com>
2022-11-14 11:19:03 +0300

    Fix MolebenBoliashih

--- app/containers/Service/Texts/MolebenBoliaschih/index.dyn.tsx
+++ app/containers/Service/Texts/MolebenBoliaschih/index.dyn.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import dateFormat from 'dateformat';
+import { makeIsDate, makeIsEasterOffsetRange } from 'domain/getDayInfo';
 
 import MolebenBoliaschihMdx from './MolebenBoliaschih.mdx';
 
-const MolebenBoliaschih = (): JSX.Element => <MolebenBoliaschihMdx />;
+
+const MolebenBoliaschih = ({ lang }) => {
+    const { date: yesterdayDate } = useParams();
+    const dateObj = new Date(yesterdayDate);
+    dateObj.setDate(dateObj.getDate() + 1);
+    const date = dateFormat(dateObj, 'yyyy-mm-dd');
+    const isDate = makeIsDate(dateObj);
+
+
+    const isEasterOffsetRange = makeIsEasterOffsetRange(date);
+
+    const props = {
+        lang,
+        date,
+        yesterdayDate,
+        isDate,
+        isEasterOffsetRange,
+    };
+
+    return <MolebenBoliaschihMdx {...props} />;
+};
+
+
 export default MolebenBoliaschih;
