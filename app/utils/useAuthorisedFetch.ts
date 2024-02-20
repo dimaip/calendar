ce3e42d540045b5e58fcec5ba2e6c58bbe02945e
Dmitri Pisarev <dimaip@gmail.com>
2024-02-20 10:32:34 +0300

    Refresh token

--- app/utils/useAuthorisedFetch.ts
+++ app/utils/useAuthorisedFetch.ts
@@ -13,19 +13,35 @@ export const useAuthorisedFetch = () => {
         method?: 'GET' | 'POST';
     }): Promise<T | null> => {
         if (token) {
-            return fetch(`${process.env.API_HOST}${url}`, {
-                headers: { Authorization: `Bearer ${token}` },
-                method,
-                ...(body
-                    ? {
-                          body: JSON.stringify(body),
-                      }
-                    : {}),
-            }).then((resp) => {
+            const doFetch = async (token: string) =>
+                fetch(`${process.env.API_HOST}${url}`, {
+                    headers: { Authorization: `Bearer ${token}` },
+                    method,
+                    ...(body
+                        ? {
+                              body: JSON.stringify(body),
+                          }
+                        : {}),
+                });
+            return doFetch(token).then(async (resp) => {
                 if (!resp.ok && resp.status === 401) {
-                    console.warn('Bad token, signing out');
-                    void auth.signOut();
-                    throw Error('Bad Token');
+                    console.warn('Expried, re-trying');
+                    const user = await auth?.userManager?.signinSilent();
+                    const newToken = user?.id_token;
+                    if (!newToken) {
+                        console.warn('Bad token recieved, signing out');
+                        void auth.signOut();
+                        throw Error('Bad Token, failed to refresh');
+                    }
+
+                    return doFetch(newToken).then((resp) => {
+                        if (!resp.ok && resp.status === 401) {
+                            console.warn('Bad token, signing out');
+                            void auth.signOut();
+                            throw Error('Bad Token, new token failed');
+                        }
+                        return resp.json() as T;
+                    });
                 }
                 return resp.json() as T;
             });
