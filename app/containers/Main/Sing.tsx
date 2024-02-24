34998bdb056fde93200f8a970adc3045a5f6b729
Dmitri Pisarev <dimaip@gmail.com>
2024-02-24 20:04:56 +0300

    Fix the link

--- app/containers/Main/Sing.tsx
+++ app/containers/Main/Sing.tsx
@@ -5,7 +5,7 @@ import TagManager from 'react-gtm-module';
 const Sing = (): JSX.Element => {
     return (
         <a
-            href="https://youtube.com/playlist?list=PLGAQ8REn_6HBNI5M0UKzQ-bP0vaQc1qpn&si=YNXivXxofr9MnwJi"
+            href="https://youtube.com/playlist?list=PLGAQ8REn_6HC2pvP0pwmgL5b0YDGpW5or&si=jz8YHVWCgRg9fI20"
             target="_blank"
             title="Песнопения на русском языке"
             className={css`
