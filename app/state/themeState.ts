1223ed9572abb0b33d9a16442a6f6a7f36e1425a
Dmitri Pisarev <dimaip@gmail.com>
2025-01-29 19:07:34 +0300

    Fallback to system theme state

--- app/state/themeState.ts
+++ app/state/themeState.ts
@@ -4,8 +4,8 @@ import { persistAtom } from './persistAtom';
 
 const themeState = atom({
     key: 'themeState',
-    default: false,
-    effects: [persistAtom(false)],
+    default: null,
+    effects: [persistAtom(null)],
 });
 
 export default themeState;
