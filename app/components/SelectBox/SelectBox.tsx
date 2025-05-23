7fd82edf6eafd07241800c806a234218c5d11bff
Dmitri Pisarev <dimaip@gmail.com>
2025-05-23 09:14:24 +0400

    Rework Select

--- app/components/SelectBox/SelectBox.tsx
+++ app/components/SelectBox/SelectBox.tsx
@@ -5,6 +5,7 @@ import { useSelect } from 'downshift';
 import Loader from 'components/Loader/Loader';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
+import { truncateText } from 'utils/truncateText';
 
 const SelectBox = React.memo(
     ({
@@ -82,7 +83,7 @@ const SelectBox = React.memo(
                     <span>
                         {items?.length ? (
                             <div style={{ display: 'flex', alignItems: 'center' }}>
-                                {selectedItem?.shortLabel || selectedItem?.label}{' '}
+                                {truncateText(selectedItem?.shortLabel || selectedItem?.label || '', 17)}{' '}
                                 {showChevron &&
                                     (isOpen ? (
                                         <ExpandLess style={{ fontSize: '1rem', marginLeft: 3 }} />
@@ -100,44 +101,66 @@ const SelectBox = React.memo(
                         <div
                             className={css`
                                 overflow: scroll;
-                                max-height: 90vh;
-                                max-width: 450px;
                                 background: ${theme.colours.bgGrayLight};
-                                position: absolute;
                                 border: 1px solid ${theme.colours.lineGray};
-                                border-radius: 8px;
+
                                 @media (max-width: 450px) {
-                                    left: 16px;
+                                    position: fixed;
+                                    top: 0;
+                                    left: 0;
+                                    width: 100vw;
+                                    height: 100vh;
                                 }
-                                z-index: 2;
+                                @media (min-width: 451px) {
+                                    max-height: 90vh;
+                                    max-width: 450px;
+                                    position: absolute;
+                                    border-radius: 8px;
+                                }
+                                z-index: 10000;
                             `}
                         >
                             {items.map((item, index) => (
                                 <div
                                     className={css`
-                                        padding: 6px 12px;
+                                        padding: ${item.level === 2
+                                            ? '12px 12px 0 12px'
+                                            : !item.level
+                                            ? '12px'
+                                            : '6px 12px'};
                                         padding-left: ${item.level === 3 ? 24 : 12}px;
                                         font-weight: ${selectedItem === item ? 'bold' : 'normal'};
                                         background-color: ${highlightedIndex === index
                                             ? theme.colours.bgGray
                                             : 'transparent'};
-                                        border-top: ${item.level === 2 ? `1px solid ${theme.colours.lineGray}` : ''};
+                                        border-top: ${item.level === 2 || !item.level
+                                            ? `1px solid ${theme.colours.lineGray}`
+                                            : ''};
+                                        margin-bottom: ${item.level === 3 && items[index + 1]?.level === 2
+                                            ? '8px'
+                                            : '0px'};
                                     `}
                                     key={`${item.value}${index}`}
                                     {...getItemProps({ item, index })}
                                 >
                                     <div
-                                        className={
+                                        className={`${
                                             item.level === 2
                                                 ? css`
                                                       text-transform: uppercase;
                                                       font-size: 14px;
                                                       font-weight: bold;
                                                       color: ${theme.colours.primary};
-                                                      margin-top: 8px;
+                                                      margin-top: 4px;
+                                                      margin-bottom: 4px;
                                                   `
                                                 : ''
-                                        }
+                                        } ${css`
+                                            display: block;
+                                            white-space: nowrap;
+                                            text-overflow: ellipsis;
+                                            overflow: hidden;
+                                        `}`}
                                     >
                                         {item.label}
                                     </div>
